06307c7a2e6efb0589c7ef962b81ec62c1e4c8eb
Hiro Phan <hung.pm165@gmail.com>
2021-12-26 22:21:28 +0700

    fix bug NSAttributedString loses attributes

--- ActiveLabel/ActiveLabel.swift
+++ ActiveLabel/ActiveLabel.swift
@@ -122,7 +122,7 @@ typealias ElementTuple = (range: NSRange, element: ActiveElement, type: ActiveTy
     }
     
     override open var attributedText: NSAttributedString? {
-        didSet { updateTextStorage(parseText: false) }
+        didSet { updateTextStorage() }
     }
     
     override open var font: UIFont! {
@@ -290,6 +290,10 @@ typealias ElementTuple = (range: NSRange, element: ActiveElement, type: ActiveTy
             clearActiveElements()
             let newString = parseTextAndExtractActiveElements(mutAttrString)
             mutAttrString.mutableString.setString(newString)
+            let range = (attributedText.string as NSString).range(of: newString)
+            attributedText.enumerateAttributes(in: range, options: .longestEffectiveRangeNotRequired, using: { (attribute,  range, stop) in
+                mutAttrString.addAttributes(attribute, range: range)
+            })
         }
         
         addLinkAttribute(mutAttrString)
